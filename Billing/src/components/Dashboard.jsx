fc70ebeb06f876752c8e61b231dcc48e2b6037ba
sowmiya <sowmiyanallasivam3434@gmail.com>
2024-06-25 12:54:04 +0530

    Table orders correctly added in db

--- Billing/src/components/Dashboard.jsx
+++ Billing/src/components/Dashboard.jsx
@@ -16,12 +16,12 @@ const Dashboard = () => {
         >
           <FaList className="mr-0 md:mr-2" /> <span className="hidden md:inline">MenuList</span>
         </Link>
-        <Link
+        {/* <Link
           className="flex items-center justify-center md:justify-start py-2.5 px-4 rounded transition duration-200 hover:bg-indigo-700 hover:text-white text-gray-400 w-full"
           to="/adminComponent"
         >
           <FaUserCog className="mr-0 md:mr-2" /> <span className="hidden md:inline">Admin Component</span>
-        </Link>
+        </Link> */}
         <Link
           className="flex items-center justify-center md:justify-start py-2.5 px-4 rounded transition duration-200 hover:bg-indigo-700 hover:text-white text-gray-400 w-full"
           to="/orderedData"
