fc1bb6087a46fd6c14c39246d06aaf0366d408d0
sowmiya <sowmiyanallasivam3434@gmail.com>
2024-07-06 10:34:23 +0530

    Normal setup

--- Billing/src/components/Dashboard.jsx
+++ Billing/src/components/Dashboard.jsx
@@ -33,7 +33,7 @@ const Dashboard = () => {
           to="/stock"
         >
           <FaBoxes className="mr-0 md:mr-2" /> <span className="hidden md:inline">Stock</span>
-        </Link>
+        </Link> 
         <Link
           className="flex items-center justify-center md:justify-start py-2.5 px-4 rounded transition duration-200 hover:bg-indigo-700 hover:text-white text-gray-400 w-full"
           to="/stockdata"
